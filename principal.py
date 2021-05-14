830f23bb0499b9b00887fba25e15c9d1b39bda69
SalimVazquez <salimvzqz@gmail.com>
2021-05-14 16:37:54 -0500

    Formulario de Modelado

--- principal.py
+++ principal.py
@@ -1,15 +1,42 @@
 from tkinter import *
 
-# Create a new window 400x350
-window = Tk()
-window.title("SGA - UPCH IA")
-window.geometry("400x350")
+fields = (
+	'Población inicial',
+	'Tamaño de cadena de bits',
+	'Número de generaciones',
+	'Prob de mutación de población',
+	'Prob de mutación de individuo'
+)
 
-# Title for window
-title = Label(window, text="Modelado")
-title.config(anchor=CENTER)
-title.pack()
+def start(entries):
+	print('Start Algorithm')
+
+def makeform(root, fields):
+    title = Label(root, text="Modelado", width=20, font=("bold",20))
+    title.pack()
+    entries = {}
+    for field in fields:
+      row = Frame(root)
+      lab = Label(row, width=30, text=field+": ", anchor='w')
+      ent = Entry(row)
+      ent.insert(0,"0")
+      row.pack(side = TOP, fill = X, padx = 5 , pady = 5)
+      lab.pack(side = LEFT)
+      ent.pack(side = RIGHT, expand = YES, fill = X)
+      entries[field] = ent
+    return entries
 
 
-# Show the window
-window.mainloop()
+if __name__ == '__main__':
+   root = Tk()
+   root.title("SGA - UPCH IA")
+   root.geometry("300x300")
+   root.resizable(0,0)
+   ents = makeform(root, fields)
+   root.bind('<Return>', (lambda event, e = ents: fetch(e)))
+   b1 = Button(root, text = 'Iniciar',
+      command=(lambda e = ents: start(e)), bg="green",fg='white')
+   b1.pack(side = LEFT, padx = 5, pady = 5, expand = YES)
+   b3 = Button(root, text = 'Quit', command = root.quit, bg="red",fg='white')
+   b3.pack(side = LEFT, padx = 5, pady = 5, expand = YES)
+   root.mainloop()
