b57989c2f07ac87d9a6f16651e543fe709dfaa5b
SalimVazquez <salimvzqz@gmail.com>
2021-05-14 13:27:23 -0500

    Window created

---
+++ principal.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# Create a new window 400x350
+window = Tk()
+window.title("SGA - UPCH IA")
+window.geometry("400x350")
+
+# Title for window
+title = Label(window, text="Modelado")
+title.config(anchor=CENTER)
+title.pack()
+
+
+# Show the window
+window.mainloop()
