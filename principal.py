014106e48b5964e7ab9680db3f2a1655123837b8
SalimVazquez <salimvzqz@gmail.com>
2021-05-16 22:12:12 -0500

    Se incluyo eje Y, calculo de fitness, suma y prom de fitness

--- principal.py
+++ principal.py
@@ -5,8 +5,10 @@ import math
 
 # globals
 root = Tk()
-individues = []
+bits = []
+individuals = []
 selection = []
+contPob = 0
 
 fields = (
     'Población inicial', # int
@@ -15,12 +17,20 @@ fields = (
     'Número de generaciones', # int
     'Rango mínimo de X', # float 
     'Rango máximo de X', # float
+    'Rango mínimo de Y', # float 
+    'Rango máximo de Y', # float
     'Prob de mutación de bits', # float
     'Prob de mutación de individuo' # float
 )
 
+def findFitness(x, y):
+    return x**2 + y**3 # z = f(x) = x² + y³
+
 def findX(a, Dx, decimal):
     return a + (decimal * Dx)
+
+def findY(a, Dy, decimal):
+    return a + (decimal * Dy)
 
 def printList(list):
     for i in range(len(list)):
@@ -36,22 +46,40 @@ def createIndividuals(pob, bits):
         aux.append(cadena)
     return aux
 
-def inicializacion(inp):
-    global individues
+def evaluation():
+    global individuals
     global selection
-    rango = abs(int(inp['Rango máximo de X'].get()) - int(inp['Rango mínimo de X'].get()))
-    Dx = (rango / len(inp['Tamaño de cadena de bits']).get())
-    info = 'X mínimo: ', inp['Rango mínimo de X'].get(), ', X máximo: ', inp['Rango máximo de X'].get(), ', Dx: ', Dx
+    totFitness = 0
+    promFitness = 0
+
+    for i in range(len(individuals)):
+        totFitness += individuals[i]['Fitness']
+    print('Sum fitness: ',totFitness)
+    print('Prom fitness: ',(totFitness/len(individuals)))
+
+def inicializacion(inp):
+    global bits
+    global individuals
+    global contPob
+    rangoX = abs(int(inp['Rango máximo de X'].get()) - int(inp['Rango mínimo de X'].get()))
+    Dx = (rangoX / int(inp['Tamaño de cadena de bits'].get()))
+    rangoY = abs(int(inp['Rango máximo de Y'].get()) - int(inp['Rango mínimo de Y'].get()))
+    Dy = (rangoY / int(inp['Tamaño de cadena de bits'].get()))
+    info = 'X mínimo: ', inp['Rango mínimo de X'].get(), ', X máximo: ', inp['Rango máximo de X'].get(), ', Dx: ', Dx, ', Dy: ', Dy
     print(info)
-    individues = createIndividuals(int(inp['Población inicial'].get()), int(inp['Tamaño de cadena de bits'].get()))
+    bits = createIndividuals(int(inp['Población inicial'].get()), int(inp['Tamaño de cadena de bits'].get()))
     for i in range(int(inp['Población inicial'].get())):
-        auxX = findX(int(inp['Rango mínimo de X'].get()), Dx, int(individues[i],2))
-        dictPob = {'ID':i+1, 'bits': individues[i], 'decimal': int(individues[i], 2), 'X': auxX, 'Fitness': 0}
-        selection.append(dictPob)
-    printList(selection)
+        auxX = findX(int(inp['Rango mínimo de X'].get()), Dx, int(bits[i],2))
+        auxY = findY(int(inp['Rango mínimo de Y'].get()), Dy, int(bits[i],2))
+        auxFitness = findFitness(auxX, auxY)
+        dictPob = {'ID':i+1, 'bits': bits[i], 'decimal': int(bits[i], 2), 'X': auxX, 'Y': auxY, 'Fitness': auxFitness, 'Prob': 0, 'Conteo': 0}
+        individuals.append(dictPob)
+        contPob += 1
+    printList(individuals)
 
 def start(entries):
     inicializacion(entries)
+    evaluation()
 
 def validModelation(input):
     if input.isdigit():
@@ -77,7 +105,7 @@ def makeform(root, fields):
 
 if __name__ == '__main__':
     root.title("SGA - UPCH IA")
-    root.geometry("300x400")
+    root.geometry("300x450")
     root.resizable(0,0)
     ents = makeform(root, fields)
     root.bind('<Return>', (lambda event, e = ents: fetch(e)))
